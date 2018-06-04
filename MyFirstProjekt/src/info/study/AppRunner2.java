92c853a4ae965862d4e930c8416b4dbf6538846e
Jktu911 <Jktu911@DESKTOP-MGMJJ5H>
2018-06-04 22:09:56 +0300

    new class

--- MyFirstProjekt/src/info/study/AppRunner2.java
+++ MyFirstProjekt/src/info/study/AppRunner2.java
@@ -6,11 +6,15 @@ public class AppRunner2 {
 	
 		int i = 12345;
 		
-
+		int sum = 0;
 		
-		
-		
-
-		
+		while(i != 0){
+			
+			sum += (i % 10);
+		        
+			i/=10;
+		}
+		System.out.println(sum);
 	}
+
 }
